03cbc3eb1fda24bcbdc050877f711f8b1e71f6a6
kevinzepwa <zepwakevin8@gmail.com>
2021-10-30 01:36:17 +0300

    just a minor change

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "software developer.", "blogger.", "Google Ads Expert", "designer.", "freelancer."],
+        strings: [ "software developer.", "blogger.", "Google Ads Expert.", "designer.", "freelancer."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "software developer.", "blogger.", "Google Ads Expert", "designer.", "freelancer."],
+        strings: [ "software developer.", "blogger.", "Google Ads Expert.", "designer.", "freelancer."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
